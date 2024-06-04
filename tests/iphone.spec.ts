32af9b26d64d26d5166b8749ebc89409aa20a11c
Oleksii Hryhurko <ohryhurko@multimediallc.com>
2024-06-04 13:18:14 -0500

    cleanup

--- tests/iphone.spec.ts
+++ tests/iphone.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("iPhone 12 orientation is portait-primary", async ({ page }) => {
+test("iphone orientation is portait-primary", async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   const orientation = await page.evaluate(() => screen.orientation.type);
--- tests/pixel.spec.ts
+++ tests/pixel.spec.ts
@@ -2,7 +2,7 @@ import { test, expect, devices } from "@playwright/test";
 
 test.use(devices["Pixel 5"]);
 
-test("Pixel 5 orientation is portait-primary", async ({ page }) => {
+test("pixel orientation is portait-primary", async ({ page }) => {
   await page.goto("https://playwright.dev/");
 
   const orientation = await page.evaluate(() => screen.orientation.type);
